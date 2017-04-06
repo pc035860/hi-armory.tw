c656997df2946440aba922689824c19830972caa
pc035860 <pc035860@gmail.com>
2017-04-07 02:31:47 +0800

    autocomplete utils: createIndex

---
+++ utils/createIndex.js
@@ -0,0 +1,38 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../firebase-adminsdk.json');
+
+const forEach = require('lodash/forEach');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://wow-ap-level.firebaseio.com'
+});
+
+const db = admin.database();
+
+db.ref('results').once('value', (snapshot) => {
+  const updates = {};
+  let count = 0;
+
+  forEach(snapshot.val(), (v, k) => {
+    const simpleKey = k.split(/-/).slice(0, 3).join('-');
+
+    count += 1;
+    console.log(count, 'simpleKey', simpleKey);
+
+    if (v.status === 'not found') {
+      updates[simpleKey] = null;
+    }
+    else if (v.data) {
+      updates[simpleKey] = true;
+    }
+  });
+
+  db.ref('index').update(updates)
+  .then(() => {
+    console.log('done!');
+
+    db.goOffline();
+  });
+});
