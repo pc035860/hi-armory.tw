d0b7cec30475afb3cc9b3877abdf63fbcc60d125
pc035860 <pc035860@gmail.com>
2017-05-10 01:46:51 +0800

    Fix assult api caching in sw
    - caused by cors headers
    - add exposed headers to assult api

--- functions/legionAssultTime.js
+++ functions/legionAssultTime.js
@@ -1,10 +1,16 @@
 const functions = require('firebase-functions');
-const cors = require('cors')({ origin: true });
+const cors = require('cors');
 
 const BASE = functions.config().legionassult.base;
 
 const base = parseInt(+new Date(BASE) / 1000, 10);
 const calcAssultTime = require('./utils/calcAssultTime')(base);
+
+const corsOptions = {
+  origin: true,
+  methods: 'GET',
+  exposedHeaders: 'Date'
+};
 
 module.exports = function legionAssultTime() {
   return functions.https.onRequest((req, res) => {
@@ -12,7 +18,7 @@ module.exports = function legionAssultTime() {
       res.status(403).send('Forbidden!');
     }
 
-    cors(req, res, () => {
+    (cors(corsOptions))(req, res, () => {
       /**
        * api parameters
        *
@@ -30,7 +36,13 @@ module.exports = function legionAssultTime() {
       const count = Number(req.query.count || 5);
 
       const assults = calcAssultTime(startFrom, count);
-      res.status(200).jsonp({
+
+      res
+      .status(200)
+      .set({
+        'Cache-Control': 'public, max-age=3600'
+      })
+      .jsonp({
         start: startFrom,
         count,
         assults
